c3c288b85fd0b7d5b68930ca34a6644bd1d38c0f
Aqeel <aqeelvahmed@gmail.com>
2025-04-05 21:21:09 +0100

    Create flask_server.py

---
+++ flask_server.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+@app.route('/my_webhook', methods=['POST'])
+def return_response():
+    print(request.json);
+    ## Do something with the request.json data.
+    return Response(status=200)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
